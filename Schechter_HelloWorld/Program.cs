893515f66caab499564733ccc6a7f0ea30287069
ems2168 <ems2168@rit.edu>
2023-08-29 09:48:09 -0400

    First C# Program

---
+++ Schechter_HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schechter_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ethan Schechter");
+
+
+            /* Playing around with syntax I know from JavaScript:
+             
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i <= 3)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    {
+                        Console.WriteLine("4? 5?");
+                    }
+                }
+            } */
+        }
+    }
+}
