619ee887f820f08051c85ce4cb0a239061b55607
ems2168 <ems2168@rit.edu>
2023-09-04 16:14:11 -0400

    PE3 Variables

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,103 @@
+﻿using ColorsAndNumbers.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsAndNumbers
+{
+    namespace Colors
+    {
+        enum EColors
+        {
+            red,
+            orange,
+            yellow,
+            green,
+            blue,
+            indigo,
+            violet
+        }
+    }
+    namespace Numbers
+    {
+        enum ENumbers
+        {
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven
+        }
+    }
+}
+namespace FavoriteColorAndNumber
+{
+    using ColorsAndNumbers.Colors;
+    using System.Xml;
+
+    internal class Program
+    {
+        // function in c#! outside Main
+        static void WriteMyVar(ref int nParameter)
+        {
+            
+            Console.WriteLine(nParameter);
+            nParameter = 42;
+        }
+        static void Main(string[] args)
+        {
+            // accessing the enum EColors which is nested inside two namespaces: with "using" on line 39, it's easier
+            EColors eColors;
+
+            Console.WriteLine(""); 
+
+
+            int myInt = 0;
+            int anotherInt = 2;
+            myInt = anotherInt;
+
+            string sFavoriteColor = null; // "null" can be used with strings
+            int? nFavoriteNumber = null;
+            string sFavoriteNumber = "";
+            bool bValid = false;
+
+            // function call!
+            // ref = reference, for call by reference 
+            WriteMyVar(ref myInt);
+
+            // -------------------------- NEW EXERCISE --------------------------
+
+            // prompt the user for their favorite color 
+            Console.Write("Hello! Please enter your favorite color: ");
+
+            // read the color from user, store it in variable 
+            sFavoriteColor = Console.ReadLine();
+
+            // prompt user for their favorite number 
+            Console.Write("Please enter your favorite number: ");
+
+            // read favorite number, store in variable 
+            sFavoriteNumber = Console.ReadLine();
+
+            while (nFavoriteNumber == null) 
+            {
+                try
+                {
+                    nFavoriteNumber = Convert.ToInt32(sFavoriteNumber);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer!!");
+                }
+            }
+
+            // a loop that outputs their favorite color fav num of times 
+        }
+    }
+}
+
+
---
+++ PE3Variables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// all work for this PE is done in this PE3Variables namespace
+namespace PE3Variables
+{
+    // Ethan Schechter
+    // IGME 201 - Professor Schuh
+    // PE 3: Variables and Expressions
+    // class Program: purpose is to prompt the user for 4 values, then print the product out
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "";
+            string value2 = "";
+            string value3 = "";
+            string value4 = "";
+
+            Console.WriteLine("Hi! The purpose of this is to take four of your values and then display the product, after multiplication!");
+
+            // prompt the user to enter their first value, then read and store 
+            Console.Write("Please enter your first number: ");
+            value1 = Console.ReadLine();
+
+            // prompt user for their second value, then read and store 
+            Console.Write("Please enter your second number: ");
+            value2 = Console.ReadLine();
+
+            // prompt for third value, then read and store
+            Console.Write("Please enter the third number: ");
+            value3 = Console.ReadLine();
+
+            // prompt for value #4, then read and store
+            Console.Write("Please enter your final number!: ");
+            value4 = Console.ReadLine();
+
+            // convert the strings to ints, then assign to new variables
+            int numValue1 = Convert.ToInt32(value1);
+            int numValue2 = Convert.ToInt32(value2);
+            int numValue3 = Convert.ToInt32(value3);
+            int numValue4 = Convert.ToInt32(value4);
+
+            // print out the product of those ints in their new variables 
+            Console.WriteLine("The product of your four numbers is: " + (numValue1 * numValue2 * numValue3 * numValue4));
+        }
+    }
+}
