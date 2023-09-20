1fdab21aa81abade5e0c75ee063c0c4f33d7f79b
ems2168 <ems2168@rit.edu>
2023-09-20 17:28:07 -0400

    PE8, Q9

---
+++ PE8_Q9/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q9
+{
+    static internal class Program
+    {
+        // Goal: to place double quotes around each word in a string (user inputted)
+        // Ethan Schechter, PE8, Q9
+        static void Main(string[] args)
+        {
+            // declare variables for strings
+            string userString; // orig. inputted user string 
+            string newUserString; // string after adding the "" quotes
+
+            // prompt user to type in a string, store 
+            Console.Write("Type in a string: "); 
+            userString = Console.ReadLine();
+
+            // set the new string equal to the orig, but replacing spaces with two quotes 
+            newUserString = userString.Replace(" ", "\" \"");
+
+            // print a quote at the beginning of the entire string, and one after
+            Console.WriteLine("\"" + newUserString + "\"");        
+        }
+    }
+}
