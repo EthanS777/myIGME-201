1133e059f69f372d2ff3e78d8965e05045222427
ems2168 <ems2168@rit.edu>
2023-09-27 21:28:26 -0400

    UT1, Q13

--- UT1_Q13/Program.cs
+++ UT1_Q13/Program.cs
@@ -1,38 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 // Ethan Schechter
 // Unit Test 1: Question 13
+
+// namespace: contains the struct Employee- goal: to prompt for user name, call func, increase salary if they got a raise 
 namespace UT1_Q13
 {
-    /* class Program: contains the struct Employee- goal: to prompt for user name, call func,
-     increase salary if they got a raise */
-    static internal class Program
+    // struct Employee: has public string and double, declare GiveRaise function
+    struct Employee
     {
 
-            struct Employee { 
-        
-            public string sName;
-            public double dSalary;
+        public string sName;
+        public double dSalary;
 
-            public static bool GiveRaise(string name, ref double salary)
+        // GiveRaise Increase salary by $19,999.99 if name is Ethan, return true- else, return false
+        public static bool GiveRaise(string name, ref double salary)
+        {
+            // if name is Ethan, increase salary and tell them they got a raise
+            if (name == "Ethan")
             {
-                
-            }
+                salary += 19999.99;
+                Console.WriteLine("Woohoo! You got a raise! Your new salary is: " + salary);
 
+                return true;
             }
+            // if name is not Ethan (lame), return false
+            else
+            {
+                return false;
+            }
+        }
 
+    }
+    // class Program: contains the calling of the struct  
+    static internal class Program
+    {
+        // Main: declare new instance of Employee struct, prompt user for info on sName, and call the GiveRaise func
         static void Main(string[] args)
         {
+            // new instance of struct
             Employee employeeName = new Employee();
 
-            employeeName.sName = "Ethan";
+            // prompt user for name, store in sName of the struct - declare salary as 30,000 to start
+            Console.Write("Hey there user, what's your name? : ");
+            employeeName.sName = Console.ReadLine();
             employeeName.dSalary = 30000;
 
-            employeeName.GiveRaise(employeeName.sName, ref employeeName.dSalary);
+            // call the GiveRaise function in Employee struct, passing the sName and dSalary of the struct
+            Employee.GiveRaise(employeeName.sName, ref employeeName.dSalary);
 
         }
     }
