2924a272fecb8e058214145dd75d09b4bf46f6f5
ems2168 <ems2168@rit.edu>
2023-11-01 15:38:23 -0400

    Tutorial 5

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Lifetime;
 using System.Security.Policy;
 using System.Text;
@@ -15,19 +16,34 @@ namespace Practice
         
         static void Main(string[] args)
         {
-            SortedList<string, DateTime> friendBirthdays = new SortedList<string, DateTime>();
-            friendBirthdays.Add("Joseph", new DateTime(2002, 10, 31));
-            friendBirthdays.Add("Andrew", new DateTime(2003, 02, 16));
-            friendBirthdays.Add("Violet", new DateTime(2002, 12, 08));
+            
+            // for (w, x, y), there are 11 w's, 41 x's, 21 y's 
+            Tuple<double, double, double> mathTime = new Tuple<double, double, double>(11, 41, 21);
+            SortedList<(double, double), double> mathTimer = new SortedList<(double, double), double>();
 
-            foreach (KeyValuePair<string, DateTime> valuePair in  friendBirthdays)
+            int nW = 0;
+            int nX = 0;
+            int nY = 0;
+            // for-loops for the ranges of w, x, y
+            for (double w = -2; w <= 0; w += 0.2, nW++)
             {
-                Console.WriteLine($"person[{valuePair.Key}] = {valuePair.Value}");
-            }
+                w = Math.Round(w, 1);
 
+                for (double x = 0; x <= 4; x += 0.1, nX++)
+                {
+                    x = Math.Round(x, 1);
+
+                    for (double y = -1; y <= 1; y += 0.1, nY++)
+                    {
+                        y = Math.Round(y, 1);
+
+                        double z = (4 * Math.Pow(y, 3)) + (2 * Math.Pow(x, 2)) - (8 * w) + 7;
+
+                        Console.WriteLine(Math.Round(z, 3));
+                        
+                    }
+                }
+            }
         }
     }
-
-
-
 }
