528a33aa36d0b029f26740d8ac06cf8071325266
ems2168 <ems2168@rit.edu>
2023-12-03 20:52:23 -0500

    UT3, Q3

--- UT3Q3/Form1.Designer.cs
+++ UT3Q3/Form1.Designer.cs
@@ -31,13 +31,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.q1Text = new System.Windows.Forms.Label();
             this.q1Box = new System.Windows.Forms.TextBox();
+            this.q2Text = new System.Windows.Forms.Label();
+            this.q2Box = new System.Windows.Forms.CheckedListBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.q3Text = new System.Windows.Forms.Label();
+            this.q3Radio1 = new System.Windows.Forms.RadioButton();
+            this.q3Radio2 = new System.Windows.Forms.RadioButton();
+            this.q3Radio3 = new System.Windows.Forms.RadioButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(334, 37);
+            this.label1.Location = new System.Drawing.Point(204, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(143, 32);
             this.label1.TabIndex = 0;
@@ -46,7 +54,7 @@
             // q1Text
             // 
             this.q1Text.AutoSize = true;
-            this.q1Text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.q1Text.ForeColor = System.Drawing.Color.White;
             this.q1Text.Location = new System.Drawing.Point(37, 112);
             this.q1Text.Name = "q1Text";
             this.q1Text.Size = new System.Drawing.Size(193, 16);
@@ -63,12 +71,111 @@
             this.q1Box.Size = new System.Drawing.Size(100, 22);
             this.q1Box.TabIndex = 2;
             // 
+            // q2Text
+            // 
+            this.q2Text.AutoSize = true;
+            this.q2Text.ForeColor = System.Drawing.Color.White;
+            this.q2Text.Location = new System.Drawing.Point(37, 247);
+            this.q2Text.Name = "q2Text";
+            this.q2Text.Size = new System.Drawing.Size(183, 16);
+            this.q2Text.TabIndex = 3;
+            this.q2Text.Text = "Question 2: What is your age?";
+            // 
+            // q2Box
+            // 
+            this.q2Box.BackColor = System.Drawing.Color.Yellow;
+            this.q2Box.FormattingEnabled = true;
+            this.q2Box.Items.AddRange(new object[] {
+            "<13",
+            "13-17",
+            "18-25",
+            "26-49",
+            "50-64",
+            "65+"});
+            this.q2Box.Location = new System.Drawing.Point(261, 227);
+            this.q2Box.Name = "q2Box";
+            this.q2Box.Size = new System.Drawing.Size(95, 72);
+            this.q2Box.TabIndex = 6;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.Yellow;
+            this.submitButton.ForeColor = System.Drawing.Color.Red;
+            this.submitButton.Location = new System.Drawing.Point(673, 427);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(137, 52);
+            this.submitButton.TabIndex = 7;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = false;
+            // 
+            // q3Text
+            // 
+            this.q3Text.AutoSize = true;
+            this.q3Text.ForeColor = System.Drawing.Color.White;
+            this.q3Text.Location = new System.Drawing.Point(12, 380);
+            this.q3Text.Name = "q3Text";
+            this.q3Text.Size = new System.Drawing.Size(271, 16);
+            this.q3Text.TabIndex = 8;
+            this.q3Text.Text = "Question 3: What is your employment status?";
+            // 
+            // q3Radio1
+            // 
+            this.q3Radio1.AutoSize = true;
+            this.q3Radio1.BackColor = System.Drawing.Color.Yellow;
+            this.q3Radio1.Location = new System.Drawing.Point(307, 353);
+            this.q3Radio1.Name = "q3Radio1";
+            this.q3Radio1.Size = new System.Drawing.Size(144, 20);
+            this.q3Radio1.TabIndex = 9;
+            this.q3Radio1.TabStop = true;
+            this.q3Radio1.Text = "Currently employed";
+            this.q3Radio1.UseVisualStyleBackColor = false;
+            // 
+            // q3Radio2
+            // 
+            this.q3Radio2.AutoSize = true;
+            this.q3Radio2.BackColor = System.Drawing.Color.Yellow;
+            this.q3Radio2.Location = new System.Drawing.Point(307, 390);
+            this.q3Radio2.Name = "q3Radio2";
+            this.q3Radio2.Size = new System.Drawing.Size(119, 20);
+            this.q3Radio2.TabIndex = 10;
+            this.q3Radio2.TabStop = true;
+            this.q3Radio2.Text = "Taking a break";
+            this.q3Radio2.UseVisualStyleBackColor = false;
+            // 
+            // q3Radio3
+            // 
+            this.q3Radio3.AutoSize = true;
+            this.q3Radio3.BackColor = System.Drawing.Color.Yellow;
+            this.q3Radio3.Location = new System.Drawing.Point(309, 427);
+            this.q3Radio3.Name = "q3Radio3";
+            this.q3Radio3.Size = new System.Drawing.Size(163, 20);
+            this.q3Radio3.TabIndex = 11;
+            this.q3Radio3.TabStop = true;
+            this.q3Radio3.Text = "Never been employed";
+            this.q3Radio3.UseVisualStyleBackColor = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(510, 6);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(555, 324);
+            this.webBrowser1.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
-            this.ClientSize = new System.Drawing.Size(873, 450);
+            this.ClientSize = new System.Drawing.Size(1077, 548);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.q3Radio3);
+            this.Controls.Add(this.q3Radio2);
+            this.Controls.Add(this.q3Radio1);
+            this.Controls.Add(this.q3Text);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.q2Box);
+            this.Controls.Add(this.q2Text);
             this.Controls.Add(this.q1Box);
             this.Controls.Add(this.q1Text);
             this.Controls.Add(this.label1);
@@ -84,6 +191,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label q1Text;
         private System.Windows.Forms.TextBox q1Box;
+        private System.Windows.Forms.Label q2Text;
+        private System.Windows.Forms.CheckedListBox q2Box;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label q3Text;
+        private System.Windows.Forms.RadioButton q3Radio1;
+        private System.Windows.Forms.RadioButton q3Radio2;
+        private System.Windows.Forms.RadioButton q3Radio3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- UT3Q3/Form1.cs
+++ UT3Q3/Form1.cs
@@ -12,27 +12,161 @@ namespace UT3Q3
 {
     public partial class Form1 : Form
     {
+        // n and k for counter
+        int n = 0;
+        int k = 0;
         public Form1()
         {
             InitializeComponent();
 
-            this.q1Box.TextChanged += new EventHandler(Q1Box__TextChanged);
+            // using the WebBrowser control with Internet Explorer
+            try
+            {
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\WOW6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                key.SetValue(Application.ExecutablePath.Replace(Application.StartupPath + "\\", ""), 12001, Microsoft.Win32.RegistryValueKind.DWord);
+                key.Close();
+            }
+            catch
+            {
+            }
 
-           
+            // set initial state, make Q2/Q3 hidden
+            q2Text.Visible = false;
+            q2Box.Visible = false;
+            q3Text.Visible = false;
+            q3Radio1.Visible = false;
+            q3Radio2.Visible = false;
+            q3Radio3.Visible = false;
+            submitButton.Enabled = true;
+            webBrowser1.Visible = false;
+            
+            // event handlers
+            this.q1Box.TextChanged += new EventHandler(Q1Box__TextChanged);
+            this.q2Box.MouseEnter += new EventHandler(Q2Box__MouseEnter);
+            this.q2Box.MouseLeave += new EventHandler(Q2Box__MouseLeave);
+            this.q3Radio1.CheckedChanged += new EventHandler(Q3Radio1__CheckedChanged);
+            this.q3Radio2.CheckedChanged += new EventHandler(Q3Radio2__CheckedChanged);
+            this.q3Radio3.CheckedChanged += new EventHandler(Q3Radio3__CheckedChanged);
+            this.submitButton.Click += new EventHandler(SubmitButton__Click);
+            this.submitButton.MouseEnter += new EventHandler(SubmitButton__MouseEnter);
+            this.submitButton.MouseLeave += new EventHandler(SubmitButton__MouseLeave);
         }
 
+        // if text changed in Q1 box, update location - every other key typed, show error/reset text
         private void Q1Box__TextChanged(object sender, EventArgs e)
         {
+            
+            n++;
 
-            q1Box.Location = new Point();
+            q1Box.Location = new Point(n * 100, n * 50);
+
+               if (q1Box.Location.X > 600)
+            {
+                q1Box.Location = new Point();
+
+            }
+            
+            
             
             
             q1Box.RightToLeft = RightToLeft.Yes;
 
-            if (q1Box.Text.Contains("a") || q1Box.Text.Contains("e") || q1Box.Text.Contains("i"))
+            // show error & reset text 
+            if (n % 2 == 0)
             {
                 MessageBox.Show("That letter doesn't exist. ERROR");
+                MessageBox.Show("INVALID LETTER");
+
+                q1Box.ResetText();
+                q2Box.Visible = true;
+                q2Text.Visible = true;
+                q1Box.Location = new Point(261, 109);
             }
+            // after 2 letters typed, show question 2 
+            else
+            {
+                q2Box.Visible = false;
+                q2Text.Visible = false;
+            }
+            
+        }
+
+        // when mouse enters Q2 box, change to new point and remove visibility - if mouse enters more than 3 times, show Q3
+        private void Q2Box__MouseEnter(object sender, EventArgs e)
+        {
+            n++;
+            k++;
+
+            q2Box.Location = new Point(n, n * 30);
+            q2Box.Visible = false;
+
+            if (k > 3)
+            {
+                q3Text.Visible = true;
+                q3Radio1.Visible = true;
+                q3Radio2.Visible = true;
+                q3Radio3.Visible = true;
+                q2Box.Location = new Point(261, 184);
+            }
+       
+        }
+
+        // when mouse leaves Q2 box, change visibility to true
+        private void Q2Box__MouseLeave(object sender, EventArgs e)
+        {
+            n++;
+
+            q2Box.Visible = true;
+           // q2Box.Location = new Point(n* 50, n);
+        }
+
+        // when Q3 first radio button is changed
+        private void Q3Radio1__CheckedChanged(object sender, EventArgs e)
+        {
+            q3Radio1.Visible = false;
+            q3Radio2.Visible = true;
+            q3Radio3.Visible = true;
+        }
+
+        // when Q3 second radio button is changed
+        private void Q3Radio2__CheckedChanged(object sender, EventArgs e)
+        {
+            q3Radio1.Visible = true;
+            q3Radio2.Visible = false;
+            q3Radio3.Visible = true;
+        }
+
+        // when Q3 third radio button is changed, make ALL visible, including Web Browser, change URL to show GIF
+        private void Q3Radio3__CheckedChanged(object sender, EventArgs e)
+        {
+            q3Radio1.Visible = true;
+            q3Radio2.Visible = true;
+            q3Radio3.Visible = true;
+
+            webBrowser1.Visible = true;
+            webBrowser1.Url = new Uri("https://media.giphy.com/media/mcH0upG1TeEak/giphy.gif");
+        }
+
+        // when submit button clicked, show error
+        private void SubmitButton__Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show("'Submit' not found. ERROR");
+            }
+
+        }
+
+        // when mouse enters submit button, disable it
+        private void SubmitButton__MouseEnter(object sender, EventArgs e)
+        {
+            submitButton.Enabled = false;
+        }
+        
+        // when mouse leaves exit button, enable it
+        private void SubmitButton__MouseLeave(object sender, EventArgs e)
+        {
+            submitButton.Enabled = true;
         }
     }
 }
